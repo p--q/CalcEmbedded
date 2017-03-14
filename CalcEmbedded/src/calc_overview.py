dde0626b77c6c46f678e1c39323edd1ce13de993
pq <pq.blogspot@gmail.com>
2017-03-14 17:20:20 +0900

    インデントを修正。

--- CalcEmbedded/src/calc_overview.py
+++ CalcEmbedded/src/calc_overview.py
@@ -1,16 +1,16 @@
 def calc_overview():
-     doc = XSCRIPTCONTEXT.getDocument()
-     if doc.supportsService("com.sun.star.sheet.SpreadsheetDocument"):
-         sheets = doc.getSheets()
-         sheet = sheets.getByIndex(0)
-         cellrange = sheet.getCellRangeByPosition(0,0,2,5)
-         cellrange.getCellByPosition(0,0).setString(u'みかん')
-         cellrange.getCellByPosition(1,0).setString(u'りんご')
-         subrange = cellrange.getCellRangeByPosition(0,1,2,5)
-         rangeAddress = subrange.RangeAddress
-         for i in range(rangeAddress.EndRow - rangeAddress.StartRow):
-             for j in range(rangeAddress.EndColumn - rangeAddress.StartColumn):
-                 subrange.getCellByPosition(j,i).setValue((i+1)*(j+2))
+    doc = XSCRIPTCONTEXT.getDocument()
+    if doc.supportsService("com.sun.star.sheet.SpreadsheetDocument"):
+        sheets = doc.getSheets()
+        sheet = sheets.getByIndex(0)
+        cellrange = sheet.getCellRangeByPosition(0, 0, 2, 5)
+        cellrange.getCellByPosition(0, 0).setString(u'みかん')
+        cellrange.getCellByPosition(1, 0).setString(u'りんご')
+        subrange = cellrange.getCellRangeByPosition(0, 1, 2, 5)
+        rangeAddress = subrange.RangeAddress
+        for i in range(rangeAddress.EndRow - rangeAddress.StartRow):
+            for j in range(rangeAddress.EndColumn - rangeAddress.StartColumn):
+                subrange.getCellByPosition(j, i).setValue((i + 1) * (j + 2))
 if __name__ == "__main__":
     import unopy
     XSCRIPTCONTEXT = unopy.connect()
